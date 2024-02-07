d26f6a913eb3b596e2860e975cd3e61972e246ed
Louis-Hochard <louis-hochard@hotmail.fr>
2024-02-07 14:02:58 +0100

    création de l'entité classe avec mise en place des ASSERT + fixtures/Fakers + exporté vers la BDD OK

--- KidiLink/src/DataFixtures/AppFixtures.php
+++ KidiLink/src/DataFixtures/AppFixtures.php
@@ -3,8 +3,9 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use Faker\Generator;
 use App\Entity\User;
+use Faker\Generator;
+use App\Entity\Classe;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -13,11 +14,14 @@ class AppFixtures extends Fixture
 {
     private Generator $faker;
     private $passwordHasher;
+
     public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
- 
+
+        $this->faker = Factory::create('fr-FR');
         $this->passwordHasher = $passwordHasher;
     }
+
     public function load(ObjectManager $manager): void
     {
         $user = new User();
@@ -31,6 +35,13 @@ class AppFixtures extends Fixture
         $user->setPassword($hashedPassword);
         $manager->persist($user);
         
+        for ($i=0; $i < 10 ; $i++) { 
+            $classe = new Classe();
+            $classe->setName($this->faker->word());
+            $classe->setAnneeScolaire($this->faker->year());
+            $manager->persist($classe);
+        }
+
         $manager->flush();
     }
 
