2082a441ab1b3d84f23f9f2a276946dc7e199296
Louis-Hochard <louis-hochard@hotmail.fr>
2024-02-08 14:03:20 +0100

    récupération du commit précédent

--- KidiLink/src/Entity/Comment.php
+++ KidiLink/src/Entity/Comment.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CommentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -24,10 +26,18 @@ class Comment
             {
                 $this->created_at = new \DateTimeImmutable();
                 $this->updated_at = new \DateTimeImmutable();
+                $this->comments = new ArrayCollection();
             }
 
     #[ORM\Column]
     private ?\DateTimeImmutable $updated_at = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'comments')]
+    #[ORM\JoinColumn(onDelete: "CASCADE")]
+    private ?self $photo = null;
+
+    #[ORM\OneToMany(mappedBy: 'photo', targetEntity: self::class)]
+    private Collection $comments;
 
     public function getId(): ?int
     {
@@ -66,6 +76,48 @@ class Comment
     public function setUpdatedAt(\DateTimeImmutable $updated_at): static
     {
         $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?self
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?self $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(self $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setPhoto($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(self $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getPhoto() === $this) {
+                $comment->setPhoto(null);
+            }
+        }
 
         return $this;
     }
