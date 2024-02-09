1e1cbb799e4f770786ce67346af0e1db56a49fcb
Louis-Hochard <louis-hochard@hotmail.fr>
2024-02-09 14:56:00 +0100

    Réalisation du AlbumController

---
+++ KidiLink/src/Controller/AlbumController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AlbumRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class AlbumController extends AbstractController
+{
+
+    #[Route('/api/albums', name: 'api_albums', methods: ['GET'])]
+    public function index(AlbumRepository $albumRepository): JsonResponse
+    {
+        //Recup les données pour affichage des albums.
+        $albums = $albumRepository->findAll();
+        return $this->json($albums, 200, [], ['groups' => 'get_albums_collection', 'get_album_item']);
+    }
+
+    }
+
+
+
--- KidiLink/src/Entity/Album.php
+++ KidiLink/src/Entity/Album.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\AlbumRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\AlbumRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: AlbumRepository::class)]
 class Album
@@ -14,12 +15,15 @@ class Album
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get_albums_collection', 'get_album_item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get_albums_collection', 'get_album_item'])]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups(['get_albums_collection', 'get_album_item'])]
     private ?string $description = null;
 
     #[ORM\Column]
@@ -41,6 +45,7 @@ class Album
 
     #[ORM\ManyToOne(inversedBy: 'albums')]
     #[ORM\JoinColumn(nullable: false, onDelete: "CASCADE")]
+    #[Groups(['get_albums_collection', 'get_album_item'])]
     private ?Classe $classe = null;
     
 
