126bc6e729b7d93c0a306678766becda1dd20e9e
REVERTEElodie <elodie.reverte@oclock.school>
2024-02-13 15:09:40 +0100

    install nelmio

--- KidiLink/config/bundles.php
+++ KidiLink/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
