12072de760925a6236271c2595e4e88e362689c5
Louis-Hochard <louis-hochard@hotmail.fr>
2024-02-14 15:59:47 +0100

    remise des annotation groups dans users

--- KidiLink/src/Entity/User.php
+++ KidiLink/src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
@@ -13,21 +14,26 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get_users_collection', 'get_user_item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Groups(['get_users_collection', 'get_user_item'])]
     private ?string $email = null;
 
     #[ORM\Column]
+    #[Groups(['get_users_collection', 'get_user_item'])]
     private array $roles = [];
 
     #[ORM\Column]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get_users_collection', 'get_user_item'])]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get_users_collection', 'get_user_item'])]
     private ?string $lastname = null;
 
     public function getId(): int
